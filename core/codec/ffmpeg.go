e1938ddd8c8b11d528c0eacbf184ffa07a59329a
Renol P. H <reshifr@gmail.com>
2024-01-27 21:40:41 +0700

    fix: remove non-injectionable dependencies

---
+++ core/codec/ffmpeg.go
@@ -0,0 +1,71 @@
+package codec
+
+import (
+	// "encoding/json"
+	// "fmt"
+	// "log"
+	// "strings"
+
+	"github.com/reshifr/play/core/cli"
+	// "github.com/reshifr/play/core/entity"
+	// "github.com/reshifr/play/core/entity"
+)
+
+type FFmpeg struct {
+	cmd *cli.Cmd
+}
+
+func OpenFFmpeg(
+	cache cli.LRU,
+	handler cli.OSCmdHandler,
+) (ffmpeg *FFmpeg) {
+	cmd := cli.OpenCmd(cache, handler)
+	ffmpeg = &FFmpeg{cmd: cmd}
+	return ffmpeg
+}
+
+func (ffmpeg *FFmpeg) Nothing() {}
+
+// func (ffmpeg *FFmpeg) GetMusic(path string) (music *entity.Music, ok bool) {
+// 	music = &entity.Music{}
+// 	ok = setTags(c, path, music)
+// 	if !ok {
+// 		return nil, false
+// 	}
+// 	return music, true
+// }
+
+// func setTags(c *cli.Cli, path string, music *entity.Music) (ok bool) {
+// 	output, code := c.Exec(
+// 		"ffprobe",
+// 		"-v", "quiet",
+// 		"-print_format", "json",
+// 		"-show_entries", "format_tags",
+// 		path
+// 	)
+// 	if code != cli.SUCCESS {
+// 		return false
+// 	}
+
+// 	// var data map[string]interface{}
+// 	// if err := json.Unmarshal(output, &data); err != nil {
+// 	// 	log.Fatalf("Codec: can not decode music tags.")
+// 	// 	return false
+// 	// }
+
+// 	// fmt.Println(data["format"]["tags"])
+
+// 	return false
+// }
+
+// func getTag(output string, code int) string {
+// 	if code != cli.SUCCESS {
+// 		return ""
+// 	}
+// 	parts := strings.Split(output, "=")
+// 	if len(parts) <= 1 {
+// 		return ""
+// 	}
+// 	tag := parts[1]
+// 	return tag
+// }
--- play.go
+++ play.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/hashicorp/golang-lru/v2"
+	"github.com/reshifr/play/core/cli"
 	"github.com/reshifr/play/core/codec"
 )
 
 func main() {
-	music, ok := codec.GetFlac("build/sia.flac")
-	if !ok {
-		fmt.Println("ERROR!")
-		return
+	cache, err := lru.New[string, string](10)
+	if err != nil {
+		fmt.Println("cache error!")
+		os.Exit(1)
 	}
-	fmt.Println(*music)
+	handler := cli.OSCmdHandler{
+		Command: func(bin string, args ...string) cli.OSCmd {
+			return exec.Command(bin, args...)
+		},
+		Clearenv: os.Clearenv,
+	}
+	ffmpeg := codec.OpenFFmpeg(cache, handler)
+	ffmpeg.Nothing()
+	// music, ok := codec.GetFlac("build/sia.flac")
+	// if !ok {
+	// 	fmt.Println("ERROR!")
+	// 	return
+	// }
+	// fmt.Println(*music)
+
+	// cache, ok := lru.New[string, string](100)
+	// var a exec.Cmd
+	// a.Output()
 }
