cf13180c7fc8c18c12a50f122f6a11b65e5a1632
Renol P. H <reshifr@gmail.com>
2024-02-02 19:45:52 +0700

    fix: remove generic on `Pad` interface

--- core/crypt/pad.go
+++ core/crypt/pad.go
@@ -1,11 +1,9 @@
 package crypt
 
-import "github.com/reshifr/play/core"
-
 const (
 	PAD_RNG_ERROR = 1
 )
 
-type Pad[Rng core.IRng] interface {
+type Pad interface {
 	Add(block []byte, aligned uint8) (padded []byte, ok bool)
 }
